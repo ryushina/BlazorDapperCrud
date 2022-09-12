ab5db821adee58e17687c64c0d4c42e3f8a2e548
ryushina <rustanlacanilao@gmail.com>
2022-09-13 06:18:47 +0800

    Add project files.

---
+++ BlazorDapperCrud/Data/SqlConnectionConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDapperCrud.Data
+{
+    public class SqlConnectionConfiguration
+    {
+        public SqlConnectionConfiguration(string value) => Value = value;
+        public string Value { get; }
+    }
+}
